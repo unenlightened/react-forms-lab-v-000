7c8a00b6d7cd831a20e790b781c6ca465d12a9e8
Chely Ho <adept_inept@yahoo.com>
2018-09-24 23:54:42 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -11,7 +11,7 @@ class LoginForm extends React.Component {
 
   handleChange = event => {
     this.setState({
-      username: event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
