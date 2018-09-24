a0288eac62c6e630b2ada6c3582128397e6231e7
Chely Ho <adept_inept@yahoo.com>
2018-09-24 23:43:09 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -19,6 +19,7 @@ class TwitterMessage extends React.Component {
       <div>
         <strong>Your message:</strong>
         <input type="text" onChange={this.handleChange} value={this.state.message}/>
+        <p>Characters left: {this.state.charactersLeft}</p>
       </div>
     );
   }
