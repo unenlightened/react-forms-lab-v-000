98370edcb8f736bb4f3f2acfc249923cfedc1f4f
Chely Ho <adept_inept@yahoo.com>
2018-09-24 23:45:54 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       message: '',
       charsLeft: this.props.maxChars
